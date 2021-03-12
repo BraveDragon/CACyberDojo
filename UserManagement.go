f6ecf0ad7fa4a3308793dfcceb9f159c73ec667c
Bravedragon <masa010515@hera.eonet.ne.jp>
2021-03-12 16:26:14 +0900

    ユーザー追加の処理を追加

--- UserManagement.go
+++ UserManagement.go
@@ -1,12 +1,16 @@
 package main
 
+//ユーザー関連の処理を行う
+
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
 	"crypto/ed25519"
 	"encoding/hex"
+	"encoding/json"
 	"time"
 
 	"github.com/go-gorp/gorp"
@@ -40,8 +44,13 @@ func main() {
 	routeCreater.Headers("X-Requested-With", "XMLHttpRequest")
 
 	//エンドポイントを用意
+	//ユーザー作成
 	routeCreater.HandleFunc("/user/create/{name}", userCreate).Methods("POST").Queries("name", "{name}")
+	//ユーザーサインイン
+	routeCreater.HandleFunc("/user/signIn", userSignIn).Methods("GET")
+	//ユーザー情報取得
 	routeCreater.HandleFunc("/user/get", userGet).Methods("GET")
+	//ユーザー情報更新
 	routeCreater.HandleFunc("/user/update", userUpdate).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8080", routeCreater))
 
@@ -50,45 +59,87 @@ func main() {
 func userCreate(w http.ResponseWriter, r *http.Request) {
 	//パスパラメーターから新規ユーザー名を取得
 	value := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	//ユーザー名
+	//新規ユーザー
 	newUser := User{}
+
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		//bodyの構造がおかしい時はエラーを返す
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	dbHandler, _ := DBMap.Begin()
+	//TODO: DBからユーザー名を検索(既に同じのがあったらエラーを吐く)
+
+	w.WriteHeader(http.StatusOK)
+
+	//ユーザー名
 	newUser.name = value["name"]
+
 	//IDはUUIDで生成
 	UUID, _ := uuid.NewUUID()
 	newUser.id = UUID.String()
-	now := time.Now()
+
 	//ここから認証トークン生成部
 	//認証トークンの生成方法は以下のサイトを参考にしている
 	//URL: https://qiita.com/GpAraki/items/801cb4654ce109d49ec9
 	//ユーザーIDから秘密鍵生成用のシードを生成
 	b, _ := hex.DecodeString(newUser.id)
 	privateKey := ed25519.PrivateKey(b)
+	//リフレッシュのことを考えて秘密鍵をDBに保存
 	newUser.privateKey = privateKey
 
+	//DBに追加＋反映
+	dbHandler.Insert(newUser)
+	dbHandler.Commit()
+	//全て終わればメッセージを出して終了
+	w.Write([]byte(fmt.Sprintf("User %s created", newUser.name)))
+
+}
+
+func userSignIn(w http.ResponseWriter, r *http.Request) {
+	//ユーザー
+	user := User{}
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		//bodyの構造がおかしい時はエラーを返す
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	now := time.Now()
+	expiration := time.Now().Add(30 * time.Minute)
 	jsonToken := paseto.JSONToken{
-		Audience:   "Audience",                       // 利用ユーザー判別するユニーク値
-		Issuer:     "Issuer",                         // 利用システム
-		Subject:    "WebAPI",                         // 利用機能
-		Jti:        "UUID",                           // UUID
-		Expiration: time.Now().Add(30 * time.Minute), // 失効日時
-		IssuedAt:   now,                              // 発行日時
-		NotBefore:  now,                              // 有効化日時
+		Audience:   "Audience", // 利用ユーザー判別するユニーク値
+		Issuer:     "Issuer",   // 利用システム
+		Subject:    "WebAPI",   // 利用機能
+		Jti:        "UUID",     // UUID
+		Expiration: expiration, // 失効日時
+		IssuedAt:   now,        // 発行日時
+		NotBefore:  now,        // 有効化日時
 	}
 
 	jsonToken.Set("KEY", "VALUE")
 	footer := "FOOTER"
 	tokenCreator := paseto.NewV2()
 	//トークンを生成
-	token, _ := tokenCreator.Sign(privateKey, jsonToken, footer)
-	newUser.token = token
-	dbHandler, _ := DBMap.Begin()
-	//DBに追加＋反映
-	dbHandler.Insert(newUser)
-	dbHandler.Commit()
+	token, err := tokenCreator.Sign(user.privateKey, jsonToken, footer)
 
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   token,
+		Expires: expiration,
+	})
+
+	//全て終わればユーザー情報表示画面へ
+	userGet(w, r)
 }
-
 func userGet(w http.ResponseWriter, r *http.Request) {
 
 }
