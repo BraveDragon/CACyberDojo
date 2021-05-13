12781d644cd276e5066162f39b5ee8299dd6783e
Bravedragon <masa010515@hera.eonet.ne.jp>
2021-05-13 15:47:38 +0900

    不要なlog.Print()を削除

--- model/gachamodel/model.go
+++ model/gachamodel/model.go
@@ -2,7 +2,6 @@ package gachamodel
 
 import (
 	"CACyberDojo/model"
-	"log"
 )
 
 //SelectGacha : idに合うガチャの中身を抽出.
@@ -11,6 +10,5 @@ func SelectGacha(contents *[]Gacha, id int) error {
 	//DBと構造体を結びつける
 	dbMap.AddTableWithName(Gacha{}, "gachas")
 	_, err := dbMap.Select(contents, "SELECT * FROM gachas WHERE gachaId=?", id)
-	log.Print(err)
 	return err
 }
